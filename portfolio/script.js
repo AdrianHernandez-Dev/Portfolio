e820a75df730d3227e75f0adfff37186e8d9436d
AdrianHernandez-Dev <a.a.hernandez00@gmail.com>
2019-11-10 14:37:55 -0700

    Add files via upload

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,12 +1,46 @@
 console.log('hello')
 
-function slideBio () {
-  $('.bio').on('click', function(){
+function slideName() {
+  $('.name').on('click', function(){
     console.log("checked")
       $('html, body').animate({
-        scrollTop: $(this).offset().top
-      }, 800)
+        scrollTop: $('header').offset().top
+      }, 1000)
     });
   }
 
+function slideBio() {
+  $('.bio').on('click', function(){
+    console.log("checked")
+      $('html, body').animate({
+        scrollTop: $('.section-bio').offset().top
+      }, 1000)
+    });
+  }
+
+  function slideProject() {
+    $('.projects').on('click', function(){
+      console.log("checked")
+        $('html, body').animate({
+          scrollTop: $('.section-projects').offset().top
+        }, 1000)
+      });
+    }
+
+    function slideContact() {
+      $('.contact').on('click', function(){
+        console.log("checked")
+          $('html, body').animate({
+            scrollTop: $('.section-contact').offset().top
+          }, 1000)
+        });
+      }
+
+function scroll(){
 slideBio();
+slideProject();
+slideContact();
+slideName();
+}
+
+$(scroll)
